e26f1541a0052df31066217ce108ebbe7500de12
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-05 17:31:25 -0400

    Creando dos personas

--- Ident/Ident/Models/Picture.cs
+++ Ident/Ident/Models/Picture.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ident.Models
 {
     public class Picture
-    { f
+    { 
+        [Key]
+        public int IdentityID { get; set; }
+        [Required][Display(Name ="Nombre Completo")][StringLength(30,MinimumLength =5)]
+        public string Name { get; set; }
+        [Required]
+        public string Photo { get; set; }
+        [Required]
+        public string Sex { get; set; }
+        [EmailAddress][Display(Name="Ingrese correo valido")]
+        public string email { get; set; }
+        [Display(Name ="Cumpleaños")]
+        public DateTime birthdate { get; set; }
+
+
     }
 }
